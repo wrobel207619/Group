a1b0508f6c3157c5f8803788fdc030edeced194b
Filip Wróbel <wrobelfilip@gmail.com>
2017-03-07 15:37:45 +0100

    [*]git ignore [-] removed a bit of stuff from index

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,9 +6,13 @@ package com.mycompany.app;
  */
 public class App 
 {
+    public static int EpicSumFoo(int x, int y)
+    {
+        return (x + y) * 2;
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
     }
 }
